0d55a0ede1807afd3c572598948dfd175341c1b7
alexrashkin <r100aa@yandex.ru>
2023-12-22 00:28:20 +0300

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ python manage.py makemigrations
 
 python manage.py migrate
 
-#python manage.py load_ingredients
+python manage.py load_ingredients
 #python manage.py load_tags
 
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:80
