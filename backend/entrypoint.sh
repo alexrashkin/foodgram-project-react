4c8bab02dbf721add1ebf0339e3aeb7e50fc51b6
alexrashkin <r100aa@yandex.ru>
2024-01-07 21:08:14 +0300

    new_commit

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,10 +7,10 @@ python manage.py makemigrations
 python manage.py migrate
 
 python manage.py load_ingredients
-#python manage.py load_tags
+python manage.py load_tags
 
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:80
 
 cp -r /app/collected_static/. /backend_static/static/
 
-source load_env.sh
+#source load_env.sh
