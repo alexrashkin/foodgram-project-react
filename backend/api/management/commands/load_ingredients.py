2c760e74d31cf7e0ddc7863ff82d8783327030e8
alexrashkin <r100aa@yandex.ru>
2024-07-16 21:01:38 +0300

    last commit

--- backend/api/management/commands/load_ingredients.py
+++ backend/api/management/commands/load_ingredients.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 from django.db import transaction
 from recipes.models import Ingredient
 
+
 class Command(BaseCommand):
 
     @transaction.atomic
